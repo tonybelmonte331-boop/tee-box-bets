f350ce9c5ad1c13e2a36a9602dcbe6cbd35b67a6
Antonio Belmonte <tonybelmonte331@gmail.com>
2026-02-19 12:44:46 -0500

    fix 10985637

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tee-box-bets-v21"; // bump version anytime you change files
+const CACHE_NAME = "tee-box-bets-v22"; // bump version anytime you change files
 
 const FILES_TO_CACHE = [
 "./",
