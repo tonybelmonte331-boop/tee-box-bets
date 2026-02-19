46d8d3f25338dba8a3154ea669e78102d8b56d73
Antonio Belmonte <tonybelmonte331@gmail.com>
2026-02-18 21:04:37 -0500

    back button placemment

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tee-box-bets-v7"; // bump version anytime you change files
+const CACHE_NAME = "tee-box-bets-v8"; // bump version anytime you change files
 
 const FILES_TO_CACHE = [
 "./",
