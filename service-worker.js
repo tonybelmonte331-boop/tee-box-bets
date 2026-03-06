b9f245ecf5f91c1fce8d76ff5d3c9b38d4437f3b
Antonio Belmonte <tonybelmonte331@gmail.com>
2026-03-05 21:09:24 -0500

    fixes needed

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tee-box-bets-v104"; // bump version anytime you change files
+const CACHE_NAME = "tee-box-bets-v105"; // bump version anytime you change files
 
 const FILES_TO_CACHE = [
 "./",
