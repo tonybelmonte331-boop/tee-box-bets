4cde808c444d1099cf567817d8416ded7d1ab896
Antonio Belmonte <tonybelmonte331@gmail.com>
2026-03-12 14:44:22 -0400

    delete button fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tee-box-bets-v136"; // bump version anytime you change files
+const CACHE_NAME = "tee-box-bets-v137"; // bump version anytime you change files
 
 const FILES_TO_CACHE = [
 "./",
