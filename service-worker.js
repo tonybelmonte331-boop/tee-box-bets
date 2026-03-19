8c61e1e3f3569bee06ef0a7044481e3b0d030a71
Antonio Belmonte <tonybelmonte331@gmail.com>
2026-03-18 23:43:07 -0400

    test logo resize

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tee-box-bets-v156"; // bump version anytime you change files
+const CACHE_NAME = "tee-box-bets-v157"; // bump version anytime you change files
 
 const FILES_TO_CACHE = [
 "./",
